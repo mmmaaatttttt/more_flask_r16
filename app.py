e5af7a16a0e123e436df98459d61a6b8eb97a3bd
Matt Lane <lane.matthew@gmail.com>
2020-06-04 16:48:08 -0700

    adding ability to create course

--- app.py
+++ app.py
@@ -10,8 +10,33 @@ connect_db(app)
 
 
 @app.route("/")
+def course_redirect():
+    """redirect to course_index"""
+    return redirect("/courses")
+
+@app.route("/courses")
 def course_index():
     """ Show all courses """
 
     courses = Course.query.all()
     return render_template("course_index.html", courses=courses)
+
+
+@app.route("/courses", methods=["POST"])
+def course_create():
+    """ Handle form submit """
+
+    title = request.form.get("title")
+    description = request.form.get("description")
+    new_course = Course(title=title, description=description)
+    db.session.add(new_course)
+    db.session.commit()
+
+    return redirect("/courses")
+
+
+@app.route("/courses/new")
+def course_new():
+    """ Show form for adding a course. """
+
+    return render_template("course_new.html")
